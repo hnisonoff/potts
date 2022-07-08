81682838b3fa107a18b7be700cc6ffd7f9ecfca3
Hunter Nisonoff <hunter_nisonoff@berkeley.edu>
2022-07-07 21:33:10 -0700

    Iniital work on moving to onehot

--- potts/mcmc.py
+++ potts/mcmc.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.distributions import Categorical
+from torch.distributions import Categorical, OneHotCategorical
 from abc import ABC, abstractmethod
 
 
@@ -52,18 +52,35 @@ class ReplayBuffer(object):
         self.length = length
         self.num_cats = num_cats
         self.reinit_freq = reinit_freq
-        self.buffer = torch.randint(high=num_cats, size=(buffer_size, length))
-        if num_cats == 2:
-            self.buffer = self.buffer.to(torch.float)
+        if num_cats > 2:
+            dist = OneHotCategorical(logits=torch.ones(size=(buffer_size,
+                                                             length,
+                                                             num_cats)))
+            self.buffer = dist.sample().to(torch.float)
+        else:
+            self.buffer = torch.randint(high=num_cats,
+                                        size=(buffer_size,
+                                              length)).to(torch.float)
+        assert (self.batch_size <= self.buffer_size)
         self.inds = None
 
     def sample(self):
-        inds = torch.randint(self.buffer_size, size=(self.batch_size, ))
+        inds = torch.randperm(self.buffer_size)[:self.batch_size]
         buffer_samples = self.buffer[inds]
-        random_samples = torch.randint(high=self.num_cats,
-                                       size=(self.batch_size, self.length))
-        choose_random = (torch.rand(self.batch_size) <
-                         self.reinit_freq).int()[:, None]
+        if self.num_cats > 2:
+            dist = OneHotCategorical(logits=torch.ones(size=(self.batch_size,
+                                                             self.length,
+                                                             self.num_cats)))
+            random_samples = dist.sample().to(torch.float)
+            choose_random = (torch.rand(self.batch_size) <
+                             self.reinit_freq).int()[:, None, None]
+        else:
+            random_samples = torch.randint(high=self.num_cats,
+                                           size=(self.buffer_size,
+                                                 self.length)).to(torch.float)
+            choose_random = (torch.rand(self.batch_size) <
+                             self.reinit_freq).int()[:, None]
+
         samples = choose_random * random_samples + (
             1 - choose_random) * buffer_samples
         self.inds = inds
@@ -173,44 +190,6 @@ class UniformCategoricalSampler(CategoricalMetropolistHastingsSampler):
         f_x = -model(X)
         bs, L, A, device = self.bs, self.L, self.A, self.device
         probs = torch.ones(bs, L * A, device=device) * 1 / (L * A)
-        dist = Categorical(probs)
-        return f_x, dist
-
-
-class GWGCategoricalSampler(CategoricalMetropolistHastingsSampler):
-    '''
-    Input is size (batch_size, length)
-    X[i, j] is an integer between 0 and K
-    '''
-
-    def __init__(self, bs, L, A, device):
-        '''
-        bs: batch size
-        L: number of positions
-        A: number of categories
-        '''
-        super(GWGCategoricalSampler, self).__init__()
-        self.bs = bs
-        self.L = L
-        self.A = A
-        self.device = device
-        to_onehot = nn.Embedding(A, A).to(device)
-        to_onehot.weight = nn.Parameter(torch.eye(A, device=device),
-                                        requires_grad=False)
-        self.to_onehot = to_onehot
-
-    def compute_neg_energy_and_proposal(self, X, model):
-        bs, L, A, device = self.bs, self.L, self.A, self.device
-        X_onehot = self.to_onehot(X)
-        X_onehot.requires_grad_()
-        f_x = -model.energy_from_onehot(X_onehot)
-        grad_f_x = torch.autograd.grad(f_x.sum(), X_onehot,
-                                       retain_graph=True)[0]
-        with torch.no_grad():
-            d_tilde = (grad_f_x -
-                       (X_onehot * grad_f_x).sum(dim=-1).unsqueeze(dim=-1))
-        probs = torch.softmax(d_tilde.reshape(d_tilde.shape[0], -1) / 2,
-                              dim=-1)
         dist = Categorical(probs)
         return f_x, dist
 
@@ -329,4 +308,156 @@ class GWGBinarySampler(BinaryMetropolistHastingsSampler):
             d_tilde = -(2 * X - 1) * grad_f_x
         probs = torch.softmax(d_tilde / 2, dim=-1)
         dist = Categorical(probs)
+        return f_x, dist
+
+
+class CategoricalGibbsSampler(nn.Module):
+
+    def __init__(self, L, A, rand=False):
+        super().__init__()
+        self.L = L
+        self.A = A
+
+        self._i = 0
+
+        self._ar = 0.
+        self._hops = 0.
+        self.rand = rand
+
+    def sample(self, x, model):
+        if self.rand:
+            i = np.random.randint(0, self.L)
+        else:
+            i = self._i
+
+        logits = []
+        A = self.A
+
+        for k in range(A):
+            sample = x.clone()
+            sample_i = torch.zeros((A, ))
+            sample_i[k] = 1.
+            sample[:, i, :] = sample_i
+            lp_k = -model(sample).squeeze()
+            logits.append(lp_k[:, None])
+        logits = torch.cat(logits, 1)
+        dist = OneHotCategorical(logits=logits)
+        updates = dist.sample()
+        sample = x.clone()
+        sample[:, i, :] = updates
+        self._i = (self._i + 1) % self.L
+        self._hops = ((x != sample).float().sum(-1) / 2.).sum(-1).mean().item()
+        self._ar = self._hops
+        return sample
+
+    def sample_nsteps(self, X, model, n_steps):
+        for step in range(n_steps):
+            X = self.sample(X, model)
+        return X
+
+
+class GWGCategoricalSampler(CategoricalMetropolistHastingsSampler):
+    '''
+    Input is size (batch_size, length)
+    X[i, j] is an integer between 0 and K
+    '''
+
+    def __init__(self, bs, L, A, device):
+        '''
+        bs: batch size
+        L: number of positions
+        A: number of categories
+        '''
+        super(GWGCategoricalSampler, self).__init__()
+        self.bs = bs
+        self.L = L
+        self.A = A
+        self.device = device
+
+    def sample(self, X, model):
+        model.eval()
+        bs, L, A, device = self.bs, self.L, self.A, self.device
+        # get negative energy and proposal distribution
+        f_x, forward_proposal = self.compute_neg_energy_and_proposal(X, model)
+        # sample from proposal
+        pos_aa_f = forward_proposal.sample()
+
+        pos_f = (pos_aa_f // A)
+        aa_f = (pos_aa_f - (pos_f * A))
+        # generate full samples from position + category
+        X_f = X.scatter(dim=1,
+                        index=pos_f.unsqueeze(-1),
+                        src=aa_f.unsqueeze(-1))
+        # get original category
+        aa_r = X.gather(dim=1, index=pos_f.unsqueeze(-1)).squeeze()
+        pos_aa_r = (pos_f * A + aa_r)
+        f_x_f, reverse_proposal = self.compute_neg_energy_and_proposal(
+            X_f, model)
+        forward_log_p = forward_proposal.log_prob(pos_aa_f)
+        reverse_log_p = reverse_proposal.log_prob(pos_aa_r)
+        log_alpha = (f_x_f - f_x) + (reverse_log_p - forward_log_p)
+        log_unifs = torch.log(torch.rand(bs, device=device))
+        accept = (log_alpha >= log_unifs).long()[:, None]
+        samples = (accept * X_f) + ((1 - accept) * X)
+        model.train()
+        return samples, accept
+
+    def sample_nsteps(self, X, model, n_steps):
+        model.eval()
+        bs, L, A, device = self.bs, self.L, self.A, self.device
+        # get negative energy and proposal distribution
+        f_x, forward_proposal = self.compute_neg_energy_and_proposal(X, model)
+        acc_rate = 0
+        per_change = 0
+        X_orig = X.clone()
+        for step in range(n_steps):
+            # sample from proposal
+            changes = forward_proposal.sample()
+
+            forward_log_p = forward_proposal.log_prob(changes)
+            # reshape to (bs, L, A)
+            changes_r = changes.view(X.size())
+            # get binary indicator (bs, L) indicating which dim was changed
+            changed_ind = changes_r.sum(-1)
+            X_f = X.clone() * (1. - changed_ind[:, :, None]) + changes_r
+            # get original category
+            f_x_f, reverse_proposal = self.compute_neg_energy_and_proposal(
+                X_f, model)
+            reverse_changes = X * changed_ind[:, :, None]
+            reverse_log_p = reverse_proposal.log_prob(
+                reverse_changes.view(X.size(0), -1))
+            log_alpha = (f_x_f - f_x) + (reverse_log_p - forward_log_p)
+            log_unifs = torch.log(torch.rand(bs, device=device))
+            accept = (log_alpha >= log_unifs).float()[:, None, None]
+            X_temp = X_f.clone()
+            X_f = (accept * X_f) + ((1 - accept) * X)
+            per_change += ((X_f != X).sum(dim=1).sum() / X.shape[0]).item()
+
+            X = X_f.detach()
+            accept = accept.squeeze(-1)
+
+            probs = (accept * reverse_proposal.probs) + (
+                (1 - accept) * forward_proposal.probs)
+            forward_proposal = OneHotCategorical(probs=probs)
+
+            accept = accept.squeeze(-1)
+            f_x = (accept * f_x_f) + ((1 - accept) * f_x)
+            acc_rate += accept.sum() / accept.shape[0]
+        model.train()
+        print(
+            f"Avg Acc: {acc_rate / n_steps:.2f}, Actual Acc: {per_change / n_steps:.2f}"
+        )
+        return X
+
+    def compute_neg_energy_and_proposal(self, X, model):
+        bs, L, A, device = self.bs, self.L, self.A, self.device
+        X.requires_grad_()
+        f_x = -model(X)
+        grad_f_x = torch.autograd.grad(f_x.sum(), X, retain_graph=True)[0]
+        with torch.no_grad():
+            d_tilde = (grad_f_x - (X * grad_f_x).sum(dim=-1).unsqueeze(dim=-1))
+        probs = torch.softmax(d_tilde.reshape(d_tilde.shape[0], -1) / 2,
+                              dim=-1)
+        dist = OneHotCategorical(logits=d_tilde.reshape(d_tilde.shape[0], -1) /
+                                 2)
         return f_x, dist
--- potts/potts.py
+++ potts/potts.py
@@ -16,33 +16,24 @@ class Potts(torch.nn.Module):
         super(Potts, self).__init__()
         self.L = L
         self.A = A
-        self.emb = nn.Embedding(self.A, self.A)
-        self.emb.weight = nn.Parameter(torch.eye(A), requires_grad=False)
-
         self.h = nn.Parameter(torch.zeros(L * A))
         self.W = nn.Linear(L * A, L * A, bias=False)
         parametrize.register_parametrization(self.W, "weight", Symmetric())
 
     def pseudolikelihood(self, X):
-        X = self.emb(X)
         tmp = (-(self.W(torch.flatten(X, 1, 2)) + self.h)).reshape(
             -1, self.L, self.A)
         tmp = X * F.log_softmax(tmp, dim=2)
         return tmp.reshape(-1, self.L * self.A).sum(dim=1)
 
     def forward(self, X, beta=1.0):
-        X = self.emb(X)
-        return self.energy_from_onehot(X, beta=beta)
-
-    def energy_from_onehot(self, X_onehot, beta=1.0):
-        energy = X_onehot * (
-            (self.W(torch.flatten(X_onehot, 1, 2)) * beta / 2.0) +
-            self.h).reshape(-1, self.L, self.A)
+        energy = X * ((self.W(torch.flatten(X, 1, 2)) * beta / 2.0) +
+                      self.h).reshape(-1, self.L, self.A)
         return energy.reshape(-1, self.L * self.A).sum(dim=1)
 
-    def grad_f_x(self, X_onehot):
+    def grad_f_x(self, X):
         '''
         returns shape batch x length x num aa
         '''
-        return (-((self.W(torch.flatten(X_onehot, 1, 2)) + self.h))).reshape(
+        return (-((self.W(torch.flatten(X, 1, 2)) + self.h))).reshape(
             -1, self.L, self.A)
